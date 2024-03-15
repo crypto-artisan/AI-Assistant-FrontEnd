97c9939c2a60cba23a08126c64097d317501edf8
hightecular95908 <ronnykutter95908@gmail.com>
2024-03-15 19:26:41 +1000

    tokenCalculate

--- src/pages/Assistant.tsx
+++ src/pages/Assistant.tsx
@@ -5,21 +5,14 @@ import { faPaperclip, faPaperPlane, faDatabase, faSpinner, faCopy, faCheck, faAn
 import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import Lottie from 'react-lottie';
+import { GPTTokens } from "gpt-tokens"
 
 import animationData from '../../public/Lotties/Hand-robot.json';
-// import SubmitLoading from '../../public/Lotties/Submit-loading.json';
-// import SubmitLoading from '../../public/Lotties/loading (1).json';
-// import SubmitLoading from '../../public/Lotties/loading (2).json';
 import SubmitLoading from '../../public/Lotties/loading (3).json';
 
 const serverUrl = "https://ai-assistant-back-end-pi.vercel.app/api";
-
+const tokenLimit = 8192;
 const apikey = import.meta.env.VITE_REACT_APP_OPENAI_API_KEY;
-interface Message {
-    id: number;
-    text: string;
-    sender: string;
-}
 
 const Assistant = () => {
 
@@ -39,39 +32,93 @@ const Assistant = () => {
             preserveAspectRatio: "xMidYMid slice"
         }
     };
-
+    //instruction
+    const [instruction, setInstruction] = useState<string>("");
+    const [singlePersonPrompt, setSinglePersonPrompt] = useState<string>("");
+    const [multiPersonPrompt, setMultiPersonPrompt] = useState<string>("");
+    //tokens
+    const [usedTokens, setUsedTokens] = useState<number>(8192);
     //loading status
-    const [loading, setLoading] = useState(false);
     const [submitLoading, setSubmitLoading] = useState(false);
     const [responseExist, setResponseExist] = useState(false);
     //prompting
-    const [prompt, setPrompt] = useState<string>("");
     const [transcript, setTranscript] = useState<string>("");
     const [processType, setProcessType] = useState<string>("");
     const [peopleNumber, setPeopleNumber] = useState<string>("");
-    //gpt response
+    //Gpt response
     const [completion, setCompletion] = useState("");
-    const [messages, setMessages] = useState<Message[] | null>(null);
+    //Error message
     const [typeErrorMessage, setTypeErrorMessage] = useState("");
     const [numberErrorMessage, setNumberErrorMessage] = useState("");
     const [transcriptErrorMessage, setTranscriptErrorMessage] = useState("");
+    //import singlePersonPrompt and multiPersonPrompt from google sheet.
+    useEffect(() => {
+        let response1; //
+        const fetchData1 = async () => {
+            response1 = await fetch(`${serverUrl}/singleperson-prompt`);
+            if (!response1.ok) {
+                toast.error("Something went wrong, please try again!",
+                    {
+                        position: toast.POSITION.TOP_RIGHT,
+                        autoClose: 1500,
+                        hideProgressBar: false,
+                    })
+
+                throw new Error('Something went wrong, please try again!');
+            }
+            let data1 = await response1.json();
+            setSinglePersonPrompt(data1.result);
+        }
+        const fetchData2 = async () => {
+            let response2 = await fetch(`${serverUrl}/multiperson-prompt`);
+            if (!response2.ok) {
+                toast.error("Something went wrong, please try again!",
+                    {
+                        position: toast.POSITION.TOP_RIGHT,
+                        autoClose: 1500,
+                        hideProgressBar: false,
+                    })
+                throw new Error('Something went wrong, please try again!');
+            }
+            let data2 = await response2.json();
+            setMultiPersonPrompt(data2.result);
+        }
+        fetchData1();
+        fetchData2();
+    })
+    //calculate tokens of prompt
+    useEffect(() => {
+        if (peopleNumber.trim() == "") {
+            setInstruction("");
+        }
+        else if (peopleNumber == "1") {
+            setInstruction(`%ProcessType% is ${processType}.\n${singlePersonPrompt}`);
+        }
+        else {
+            setInstruction(`%number of people% is ${peopleNumber} and %ProcessType% is ${processType}.\n${multiPersonPrompt}`);
+        }
+        const usageInfo = new GPTTokens({
+            model: 'gpt-4',
+            messages: [
+                {
+                    role: "system", content: instruction
+                },
+                { role: "user", content: transcript },
+            ],
+        })
+        let used = usageInfo.usedTokens - 11;
+        setUsedTokens(used)
+    }, [transcript])
     //clipboard
     const [isCopied, setIsCopied] = useState(false);
+    //handle input required validation message
     useEffect(() => {
-        setPrompt(transcript);
         if (transcript.trim() !== "") setTranscriptErrorMessage("");
         if (processType.trim() !== "") setTypeErrorMessage("");
         if (peopleNumber.trim() !== "") setNumberErrorMessage("");
     }, [processType, peopleNumber, transcript])
-    useEffect(() => {
-        // setPrompt("");
-        if (messages != null)
-            setMessages([
-                ...messages,
-                { id: messages.length, text: completion, sender: "bot" },
-            ]);
-    }, [completion]);
-    // const handleLoad = async () => {
+
+    // const handleLoad = async () => {  //import transcript from google sheet
     //     setLoading(true);
     //     try {
     //         if (peopleNumber.trim() == "") {
@@ -121,15 +168,19 @@ const Assistant = () => {
     //         setLoading(false);
     //     }
     // };
+
+    //back button
     const handleBack = () => {
         setSubmitLoading(false);
         setResponseExist(false);
     }
+    //regenerate button
     const handleRegenerate = () => {
         setSubmitLoading(false);
         setResponseExist(false);
         handleSubmit();
     }
+    //submit into openai
     const handleKeyDown = (event: any) => {
         if (event.key === 'Enter' && event.shiftKey) {
             event.preventDefault(); // Prevent the default behavior
@@ -170,39 +221,7 @@ const Assistant = () => {
             return;
         }
         try {
-            let instruction;
-            if (peopleNumber == "1") {
-                let response1;
-                response1 = await fetch(`${serverUrl}/singleperson-prompt`);
-                if (!response1.ok) {
-                    toast.error("Something went wrong, please try again!",
-                        {
-                            position: toast.POSITION.TOP_RIGHT,
-                            autoClose: 1500,
-                            hideProgressBar: false,
-                        })
 
-                    throw new Error('Something went wrong, please try again!');
-                }
-                let data1 = await response1.json();
-                instruction = `%ProcessType% is ${processType}.\n${data1.result}`;
-                // console.log("people = 1 instruction", instruction);
-            }
-            else {
-                let response2 = await fetch(`${serverUrl}/multiperson-prompt`);
-                if (!response2.ok) {
-                    toast.error("Something went wrong, please try again!",
-                        {
-                            position: toast.POSITION.TOP_RIGHT,
-                            autoClose: 1500,
-                            hideProgressBar: false,
-                        })
-                    throw new Error('Something went wrong, please try again!');
-                }
-                let data2 = await response2.json();
-                instruction = `%number of people% is ${peopleNumber} and %ProcessType% is ${processType}.\n${data2.result}`;
-                // console.log("people >= 1 instruction", instruction);
-            }
             const response = await fetch(`https://api.openai.com/v1/chat/completions`, {
                 method: "POST",
                 headers: {
@@ -215,11 +234,10 @@ const Assistant = () => {
                         {
                             role: "system", content: instruction
                         },
-                        { role: "user", content: prompt },
+                        { role: "user", content: transcript },
                     ],
                 })
             });
-
             const data = await response.json();
             // console.log("response data=========================>", data);
             setResponseExist(true);
@@ -238,9 +256,7 @@ const Assistant = () => {
             console.log("responseExist--------->", responseExist);
         }
     };
-    const handleTextareaEdit = (event: any) => {
-        setTranscript(event.target.value);
-    };
+    //result copy button
     const handleCopy = async () => {
         try {
             await navigator.clipboard.writeText(completion);
@@ -251,7 +267,7 @@ const Assistant = () => {
             console.error('Failed to copy text: ', err);
         }
     };
-    //file upload
+    //add transcript from openfileform
     const handleFileUpload = (event: any) => {
         const file = event.target.files[0];
         if (file) {
@@ -375,9 +391,12 @@ const Assistant = () => {
                             </form>
                             {/* </div> */}
                             <div className="my-3">
-                                <label htmlFor="transcript" className="block text-md my-2 font-medium text-gray-700">Please copy and paste your transcript below.*</label>
+                                <div className="flex flex-row justify-between">
+                                    <label htmlFor="transcript" className="block text-md my-2 font-medium text-gray-700">Please copy and paste your transcript below.*</label>
+                                    <label htmlFor="transcript" className="block text-md my-2 font-medium text-gray-700">{usedTokens}/{tokenLimit}</label>
+                                </div>
                                 <div className='relative'>
-                                    <textarea onKeyDown={handleKeyDown} onChange={handleTextareaEdit} value={transcript} rows={4} className="resize-none overflow-y-auto w-full py-3 px-3 min-h-40 text-lg bg-[#0000] text-black dark:text-white border-[#d2d5d7] font-sans rounded-xl placeholder:text-slate-400 border-solid border-2 focus:outline-none focus-within:border-[#61c6ea]" placeholder="Enter transcript..." />
+                                    <textarea onKeyDown={handleKeyDown} onChange={(e) => setTranscript(e.target.value)} value={transcript} rows={4} className="resize-none overflow-y-auto w-full py-3 px-3 min-h-40 text-lg bg-[#0000] text-black dark:text-white border-[#d2d5d7] font-sans rounded-xl placeholder:text-slate-400 border-solid border-2 focus:outline-none focus-within:border-[#61c6ea]" placeholder="Enter transcript..." />
                                     {
                                         transcriptErrorMessage && (
                                             <div className='text-[#de1515] absolute top-[6%] right-5'>
