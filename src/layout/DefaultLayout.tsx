38b546fb6e1e700121bfb997288693e0f1b52864
hightecular95908 <ronnykutter95908@gmail.com>
2024-03-26 14:18:02 +1000

    R

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -13,14 +13,14 @@ const DefaultLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
           {/* <h1 className='self-center sm:text-[40px] mt-5 mb-10 text-[0px] dark:text-white text-[#61c6ea] font-extrabold'>Easy to Use — No Tech Skills Required</h1> */}
           {/* <img src="./src/images/wave-top.svg" height={"10px"} className='w-full' /> */}
           {/* <img src = "back-top.png" height={"10px"} className='w-[32%] sm:h-8 h-[0px] self-center mb-3' /> */}
-          <h1 className='text-center text-[32px]'>
+          {/* <h1 className='text-center text-[32px]'>
 
             hi I'm Roman.
             My Upwork account is blocked.
             Contact me by gmail.<br/>
             rkulkin47@gmail.com
 
-          </h1>
+          </h1> */}
           <main className='h-screen'>
             <div className="mx-auto max-w-screen-2xl justify-items-stretch h-full relative my-5">
               {children}
